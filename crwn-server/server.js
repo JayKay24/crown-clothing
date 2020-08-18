9fc374904ec91770c8baef321ffe41de8ab6eb5d
James Kinyua <jameskinyua590@gmail.com>
2020-08-18 19:51:11 +0300

    configure ingress-nginx load balancing service

--- crwn-server/server.js
+++ crwn-server/server.js
@@ -28,9 +28,9 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-});
+// app.get("/service-worker.js", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+// });
 
 app.post("/payment", (req, res) => {
   const body = {
